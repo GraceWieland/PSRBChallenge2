0083161f3c40b1cc5122c21a101d7da004688bb5
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 13:50:06 -0500

    trying to find the right rabbit hole

--- yourbuild.bat
+++ yourbuild.bat
@@ -1,5 +1,5 @@
 cd
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 dir /s C:\Program Files (x86)\Microsoft Visual Studio\
 ::devenv.....
 ::"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" GoinPostal.sln /Build
