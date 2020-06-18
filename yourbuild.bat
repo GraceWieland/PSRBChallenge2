97b2463027cb8b0cf12093728d9c82af951264c6
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 15:50:35 -0500

    testing a build

--- yourbuild.bat
+++ yourbuild.bat
@@ -1,5 +1,5 @@
 cd
 ::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-dir /s C:\projects\goinpostal
+::dir /s C:\projects\goinpostal
 ::devenv.....
-::"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /Build
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /Build
