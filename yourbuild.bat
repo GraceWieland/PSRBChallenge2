ee2dc637cbd7b865576fcd31161dac8d5e3000fc
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 16:16:12 -0500

    testing full path

--- yourbuild.bat
+++ yourbuild.bat
@@ -1,6 +1,6 @@
 cd
 ::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 ::dir /s C:\projects\goinpostal
-mkdir "C:\projects\goinpostal\bin\Release\netstandard1.4"
-dir /s C:\projects\goinpostal\bin\Release\
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /build Release
+mkdir "C:\projects\goinpostal\bin\Debug\netstandard1.4"
+dir /s C:\projects\goinpostal\bin\Debug\
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" "C:\projects\goinpostal\GoinPostal.sln" /Build Debug
