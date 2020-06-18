849e2f36ba12dd22d8530d95540c0f7602031ec8
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 15:57:00 -0500

    adding make dir

--- yourbuild.bat
+++ yourbuild.bat
@@ -2,4 +2,5 @@ cd
 ::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 ::dir /s C:\projects\goinpostal
 ::devenv.....
+mkdir "bin\Debug\netstandard1.4"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /Build
