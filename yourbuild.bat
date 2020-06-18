bc4e047e2bfbfaaa752e8462802973828f4b686d
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 15:30:18 -0500

    parenthesis

--- yourbuild.bat
+++ yourbuild.bat
@@ -1,5 +1,5 @@
 cd
 ::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-dir /s C:\Program Files (x86)\Microsoft Visual Studio\
+dir /s "C:\Program Files (x86)\Microsoft Visual Studio\"
 ::devenv.....
 ::"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /Build
