558a26ee8141d3d8a85231ed5eb2637ccf27bf57
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 16:12:09 -0500

    testing release with release specified

--- yourbuild.bat
+++ yourbuild.bat
@@ -1,7 +1,6 @@
 cd
 ::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 ::dir /s C:\projects\goinpostal
-::devenv.....
 mkdir "C:\projects\goinpostal\bin\Release\netstandard1.4"
 dir /s C:\projects\goinpostal\bin\Release\
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /Build
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /build Release
