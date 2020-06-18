59ebffd033efc3a2e45a7798818127bcc0969565
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 16:26:57 -0500

    output?

--- yourbuild.bat
+++ yourbuild.bat
@@ -2,5 +2,6 @@ cd
 ::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 ::dir /s C:\projects\goinpostal
 mkdir "C:\projects\goinpostal\bin\Debug\netstandard1.4"
-dir /s C:\projects\goinpostal\bin\Debug\
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" "C:\projects\goinpostal\GoinPostal.sln" /Build Debug
+::dir /s C:\projects\goinpostal\bin\Debug\
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" "C:\projects\goinpostal\GoinPostal.sln" /Build Debug /Out
+::"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" GoinPostal.sln /Build Release
