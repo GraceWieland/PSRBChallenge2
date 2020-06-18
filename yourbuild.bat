14df73a1281097b1b961a9d2e3ff9740a483b593
Will Lehman <william.p.lehman@usace.army.mil>
2020-06-18 15:59:04 -0500

    adding a test on the mkdir

--- yourbuild.bat
+++ yourbuild.bat
@@ -2,5 +2,6 @@ cd
 ::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 ::dir /s C:\projects\goinpostal
 ::devenv.....
-mkdir "bin\Debug\netstandard1.4"
+mkdir "\bin\Debug\netstandard1.4"
+dir /s C:\projects\goinpostal\bin\Debug\
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" GoinPostal.sln /Build
