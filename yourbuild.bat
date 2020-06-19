5b2ba872753be986282d79f6903de1dd26b044bf
Will Lehman <willyplehman@gmail.com>
2020-06-18 20:24:11 -0500

    Delete yourbuild.bat

--- yourbuild.bat
+++
@@ -1,7 +0,0 @@
-cd
-::%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-::dir /s C:\projects\goinpostal
-mkdir "C:\projects\goinpostal\bin\Debug\netstandard1.4"
-::dir /s C:\projects\goinpostal\bin\Debug\
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" "C:\projects\goinpostal\GoinPostal.sln" /Build Debug /Out out.txt
-::"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" GoinPostal.sln /Build Release
